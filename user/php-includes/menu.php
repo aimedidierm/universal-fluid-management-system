a05c1b64c1f457b2a1e57459aebbefbef9e77bbe
aimedidierm <aimedidiermugisha@gmail.com>
2021-08-14 23:09:27 +0200

    User pages UI

---
+++ user/php-includes/menu.php
@@ -0,0 +1,41 @@
+<body>
+<div class="header">
+<div class="row">
+<div class="col-lg-6">
+<span class="logo">Universal fluid management system</span></div>
+<div class="col-md-4 col-md-offset-2">
+<span class="pull-right top title1" >
+<a href="../php-includes/logout.php" class="log">
+    <span class="glyphicon glyphicon-log-out" aria-hidden="true">
+
+    </span>&nbsp;Signout</button>
+</a>
+</span>
+</div>
+</div>
+</div>
+<div class="bg">
+<!--navigation menu-->
+<nav class="navbar navbar-default title1">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><b>UFMS - User</b></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="dashboard.php"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span>&nbsp;Dashboard</a></li>
+        <li><a href="account.php"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span>&nbsp;Account</a></li>
+		<li class="pull-right"> <a href="../php-includes/logout.php"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;&nbsp;Signout</a></li>
+	  </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav><!--navigation menu closed-->
