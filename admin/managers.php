7e966795ce81a4d7c00f11b9c332b199054493f1
aimedidierm <aimedidiermugisha@gmail.com>
2021-08-15 07:42:56 +0200

    Admin pages UI

--- admin/managers.php
+++ admin/managers.php
@@ -3,3 +3,45 @@ require '../php-includes/connect.php';
 require 'php-includes/check-login.php';
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>UFM - Admin Managers</title>
+<link  rel="stylesheet" href="../css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="../css/bootstrap-theme.min.css"/>    
+ <link rel="stylesheet" href="../css/main.css">
+ <link  rel="stylesheet" href="../css/font.css">
+ <script src="../js/jquery.js" type="text/javascript"></script>
+
+ 
+  <script src="../js/bootstrap.min.js"  type="text/javascript"></script>
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+</head>
+
+<?php include_once 'php-includes/menu.php'; ?>
+
+
+
+<div class="container"><!--container start-->
+<div class="row">
+<div class="col-md-12">
+
+
+
+
+</div>
+</div>
+</div>
+</div>
+<!--Footer start-->
+<div class="row footer">
+<div class="col-md-3 box">
+<a href="#" target="_blank">2021 Universal fluid management system</a>
+</div>
+</div>
+<!--footer end-->
+</body>
+</html>
