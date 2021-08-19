12512a3774fcf8b2a6c1d935f242e7ccfde17a4d
aimedidierm <aimedidiermugisha@gmail.com>
2021-08-19 14:28:33 +0200

    Tank done

--- manager/dashboard.php
+++ manager/dashboard.php
@@ -28,10 +28,125 @@ require 'php-includes/check-login.php';
 <div class="container"><!--container start-->
 <div class="row">
 <div class="col-md-12">
+<!-- Styles -->
+<style>
+#chartdiv {
+  width: 25%;
+  height: 400px;
+}
 
+</style>
 
+<!-- Resources -->
+<script src="https://cdn.amcharts.com/lib/4/core.js"></script>
+<script src="https://cdn.amcharts.com/lib/4/charts.js"></script>
+<script src="https://cdn.amcharts.com/lib/4/themes/animated.js"></script>
 
+<?php
+$sql = "SELECT *  FROM manager WHERE email= ? limit 1";
+$stmt = $db->prepare($sql);
+$stmt->execute(array($_SESSION['code']));
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$tank_id = $row['tank_id'];
+// Kureba ibi mukigega
+$sql = "SELECT s.tank_id, s.level, s.volume, t.names, t.id FROM status AS s JOIN tanks AS t WHERE t.id = s.tank_id AND s.tank_id = ? limit 1";
+$stmt = $db->prepare($sql);
+$stmt->execute(array($tank_id));
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$level = $row['level'];
+$volume = $row['volume'];
+$names = $row['names'];
+if(isset($_POST['open'])){
+  // Gufungura amazi
+}
+if(isset($_POST['close'])){
+  // Gufunga amazi
+}
+?>
+<!-- Chart code -->
+<script>
+am4core.ready(function() {
 
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
+
+// Create chart instance
+var chart = am4core.create("chartdiv", am4charts.XYChart3D);
+
+chart.titles.create().text = "Universal fluid management system";
+
+// Add data
+
+chart.data = [{
+  "category": '<?php echo $names ?>',
+  "value1": '<?php echo $level ?>',
+  "value2": '<?php echo 100 - $level ?>'
+}];
+
+// Create axes
+var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "category";
+categoryAxis.renderer.grid.template.location = 0;
+categoryAxis.renderer.grid.template.strokeOpacity = 0;
+
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.renderer.grid.template.strokeOpacity = 0;
+valueAxis.min = -10;
+valueAxis.max = 110;
+valueAxis.strictMinMax = true;
+valueAxis.renderer.baseGrid.disabled = true;
+valueAxis.renderer.labels.template.adapter.add("text", function(text) {
+  if ((text > 100) || (text < 0)) {
+    return "";
+  }
+  else {
+    return text + "%";
+  }
+})
+
+// Create series
+var series1 = chart.series.push(new am4charts.ConeSeries());
+series1.dataFields.valueY = "value1";
+series1.dataFields.categoryX = "category";
+series1.columns.template.width = am4core.percent(80);
+series1.columns.template.fillOpacity = 0.9;
+series1.columns.template.strokeOpacity = 1;
+series1.columns.template.strokeWidth = 2;
+
+var series2 = chart.series.push(new am4charts.ConeSeries());
+series2.dataFields.valueY = "value2";
+series2.dataFields.categoryX = "category";
+series2.stacked = true;
+series2.columns.template.width = am4core.percent(80);
+series2.columns.template.fill = am4core.color("#000");
+series2.columns.template.fillOpacity = 0.1;
+series2.columns.template.stroke = am4core.color("#000");
+series2.columns.template.strokeOpacity = 0.2;
+series2.columns.template.strokeWidth = 2;
+
+}); // end am4core.ready()
+</script>
+
+<!-- HTML -->
+<div id="chartdiv">
+</div>
+<div>
+  <h1>
+  <?php
+  $total = $level/100 * $volume;
+  echo $total;
+  ?>
+  L
+  </h1>
+  <form role="form" method="post" >
+    <fieldset>
+    <input  type="submit"  value="Open"  name="open" class="btn btn-primary"/>
+    <input  type="submit"  value="Close" name="close" class="btn btn-danger"/>
+    </fieldset>
+  </form>
+</div>
+<br>
 </div>
 </div>
 </div>
