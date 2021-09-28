682d408299ae3d3dbed26242d51d8eac490edba7
aimedidierm <aimedidiermugisha@gmail.com>
2021-09-28 16:29:47 +0200

    Decrease lines

--- Hardware/arduinocodes/arduinocodes.ino
+++ Hardware/arduinocodes/arduinocodes.ino
@@ -63,617 +63,41 @@ void setup()
 }
 
 void loop() {
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println();
   SWserial.println("AT+SAPBR=1,1");
   delay(3000);
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println("AT+SAPBR=2,1");
   delay(3000);
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println("AT+HTTPINIT");
   delay(3000);
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println("AT+HTTPPARA=CID,1");
   delay(3000);
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println("AT+HTTPPARA=URL," + url +precent);
   delay(3000);
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println("AT+HTTPPARA=CONTENT,application/x-www-form-urlencoded");
   delay(3000);
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println("AT+HTTPDATA=192,5000");
   delay(3000);
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println("param=TestFromMySim800");
   delay(3000);
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println("AT+HTTPACTION=0");
   delay(3000);
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println("AT+HTTPREAD");
   delay(3000);
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println("AT+HTTPTERM");
   delay(3000);
-  StartbuttonState = digitalRead(Startbutton);
-  StopbuttonState = digitalRead(Stopbutton);
-  lcd.clear();
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger,LOW);
-  delayMicroseconds(2);
-  time=pulseIn(echo,HIGH);
-  distance=time*340/20000;
-  precent=100-(distance/tanksize);
-  liveliters=(precent/100)*liters;
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(liveliters);
-  lcd.print(" L");
-  lcd.setCursor(0,1);
-  if (pumpState == 0) {     
-    lcd.print("Pump OFF");
-  } else {
-    lcd.print("Pump ON");
-  }
- 
-if (StartbuttonState == HIGH) {   
-    // turn Pump on:    
-    digitalWrite(pump, HIGH);
-    pumpState = 1; 
-  } 
-if (StopbuttonState == HIGH) {  
-    // turn Pump off:    
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  
-  if (liveliters >0 && liveliters <=4) {   
-    // turn Pump off:
-  } else {
-    digitalWrite(pump, LOW);
-    pumpState = 0;
-  }
-  if (liveliters >0.6 && liveliters <=4) {   
-    // turn Pump off:
-    var=0;
-  }
-  if (liveliters <=0.6 && var==0) {   
-    SendSMS();
-  } else {
-    }
+  control();
   SWserial.println("AT+SAPBR=0,1");
   delay(3000);
 }
@@ -708,30 +132,55 @@ void SendSMS()
   lcd.print("Text Sent.");
   delay(500);
   var=1;
-  /*
-  Serial.println("Sending SMS...");               //Show this message on serial monitor
-  SWserial.println("AT+CMGF=1\r");                   //Set the module to SMS mode
-  delay(100);
-  SWserial.println("AT+CMGS=\"+250781196048\"\r");  //Your phone number don't forget to include your country code, example +212123456789"
-  delay(500);
-  SWserial.println("Balance is low 15%");       //This is the text to send to the phone number, don't make it too long or you have to modify the SoftwareSerial buffer
-  delay(500);
-  SWserial.println((char)26);// (required according to the datasheet)
-  delay(500);
-  SWserial.println();
-  Serial.println("Text Sent.");
-  delay(500); 
-  SWserial.println("Sending 2nd SMS...");               //Show this message on serial monitor
-  sim800l.print("AT+CMGF=1\r");                   //Set the module to SMS mode
-  delay(100);
-  SWserial.println("AT+CMGS=\"+250788856865\"\r");  //Your phone number don't forget to include your country code, example +212123456789"
-  delay(500);
-  SWserial.println("Balance is low 15%");       //This is the text to send to the phone number, don't make it too long or you have to modify the SoftwareSerial buffer
-  delay(500);
-  SWserial.println((char)26);// (required according to the datasheet)
-  delay(500);
-  SWserial.println();
-  SWserial.println("Text Sent.");
-  delay(500);
-   */
+}
+void control(){
+  StartbuttonState = digitalRead(Startbutton);
+  StopbuttonState = digitalRead(Stopbutton);
+  lcd.clear();
+  digitalWrite(trigger,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger,LOW);
+  delayMicroseconds(2);
+  time=pulseIn(echo,HIGH);
+  distance=time*340/20000;
+  precent=100-(distance/tanksize);
+  liveliters=(precent/100)*liters;
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(liveliters);
+  lcd.print(" L");
+  lcd.setCursor(0,1);
+  if (pumpState == 0) {     
+    lcd.print("Pump OFF");
+  } else {
+    lcd.print("Pump ON");
+  }
+ 
+  if (StartbuttonState == HIGH) {   
+    // turn Pump on:    
+    digitalWrite(pump, HIGH);
+    pumpState = 1; 
+  } 
+  if (StopbuttonState == HIGH) {  
+    // turn Pump off:    
+    digitalWrite(pump, LOW);
+    pumpState = 0;
+  }
+  
+  if (liveliters >0 && liveliters <=4) {   
+    // turn Pump off:
+  } else {
+    digitalWrite(pump, LOW);
+    pumpState = 0;
+  }
+  if (liveliters >0.6 && liveliters <=4) {   
+    // turn Pump off:
+    var=0;
+  }
+  if (liveliters <=0.6 && var==0) {   
+    SendSMS();
+  } else {
+    }
 }
